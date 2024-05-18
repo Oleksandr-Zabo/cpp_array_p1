012c1bce22a52d0c9d68246145d49c0c585f5dd0
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-05-18 15:56:56 +0300

    ex.3 lab had done

--- main.cpp
+++ main.cpp
@@ -3,7 +3,22 @@ using namespace std;
 //
 int main()
 {
-	cout << "Hello world" << endl;
+	const int size = 5;
+	int side, sum = 0, arr[size];
+	for (int i = 0; i < size; i++)
+	{
+		do {
+			cout << "Enter side "
+				<< i + 1 << " lenth: ";
+			cin >> side;
+
+		} while (side < 0);
+		arr[i] = side;
+		sum += side;
+
+	}
+	cout << "Perimeter of the pentagon: "
+		<<sum << endl;
 	system("pause");
 	return 0;
 }
