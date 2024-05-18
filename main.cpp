2fe667c8ed3a5687d617934c1296626696e6b535
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-05-18 15:50:07 +0300

    ex. 2 lab had done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
-using namespace std;
+#include <ctime>
+using namespace std; 
+
 //
 int main()
 {
-	cout << "Hello world" << endl;
+	srand(time(0));
+	const int full_mem_size = 1000;
+	int size, arr[full_mem_size];
+	do {
+		cout << "Enter size(0-1000): ";
+		cin >> size;
+	} while (size > 1000 || size < 0);
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 20 - 10;
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	for (int i = size - 1; i >= 0; i--)
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << endl;
 	system("pause");
 	return 0;
 }
