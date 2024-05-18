a077fcdc231916e2b96d2e31d7a02c5ee8b6c573
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-05-18 15:40:33 +0300

    ex.1 lab had done

--- main.cpp
+++ main.cpp
@@ -3,7 +3,18 @@ using namespace std;
 //
 int main()
 {
-	cout << "Hello world" << endl;
+	const int size = 6;
+	int sum = 0, arr[size];
+	for (int i = 0; i < size; i++)
+	{
+		cout << "Enter the profit for month "
+			<< i + 1 << " : ";
+		cin >> arr[i];
+		sum += arr[i];
+	}
+
+	cout << "In 6 months you have earned: "
+		<< sum << endl;
 	system("pause");
 	return 0;
 }
